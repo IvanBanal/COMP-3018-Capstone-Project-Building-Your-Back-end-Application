fdd111db72796b26f504f15516a72b819dce33f9
IvanBanal <ibanal@rrc.ca>
2026-04-14 11:09:24 -0500

    Completed memberSchema.ts.

--- src/api/v1/validation/bookSchema.ts
+++ src/api/v1/validation/bookSchema.ts
@@ -1,5 +1,4 @@
 import Joi from "joi";
-import { availableMemory } from "process";
 
 /**
  * Book validation schema.
---
+++ src/api/v1/validation/memberSchema.ts
@@ -0,0 +1,22 @@
+import Joi from "joi";
+
+/**
+ * Member validation schema.
+ */
+export const createMemberSchema = Joi.object({
+    name: Joi.string().min(3).max(100).required(),
+
+    email: Joi.string().email().required(),
+
+    status: Joi.string()
+        .valid("active", "inactive", "suspended")
+        .default("active"),
+
+    borrowLimit: Joi.number().integer().min(1).max(20).default(5),
+
+    phoneNumber: Joi.string().min(10).max(15).required(),
+
+    address: Joi.string().min(5).max(200).required(),
+
+    booksBorrowed: Joi.array().items(Joi.string()).default([]),
+});
