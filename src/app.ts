a21c1a3f4dab1b7c7beae7e9ae5ae77cedcdd59d
IvanBanal <ibanal@rrc.ca>
2026-04-14 11:56:39 -0500

    Adding API Routes to app.ts.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
 import express, { Express } from "express";
+import dotenv from "dotenv";
+
+// Load environment variables BEFORE your internal imports!
+dotenv.config();
+
+import cors from "cors";
+import { getCorsOptions } from "../src/api/v1/config/corsConfig";
+import { getHelmetConfig } from "../src/api/v1/config/helmetConfig";
+import setupSwagger from "../src/api/v1/config/swagger";
+import bookRoutes from "../src/api/v1/routes/bookRoutes";
+import memberRoutes from "../src/api/v1/routes/memberRoutes";
 
 // Initialize Express application
 const app: Express = express();
+
+// Apply basic Helmet security
+app.use(getHelmetConfig());
+
+app.use(cors(getCorsOptions()));
 
 // Define a route
 app.get("/", (req, res) => {
@@ -19,5 +35,12 @@ app.get("/api/v1/health", (req, res) => {
 });
 
 app.use(express.json());
+
+// API Routes.
+app.use("/api/v1/books", bookRoutes);
+app.use("/api/v1/members", memberRoutes);
+
+// Setup Swagger
+setupSwagger(app);
 
 export default app;
