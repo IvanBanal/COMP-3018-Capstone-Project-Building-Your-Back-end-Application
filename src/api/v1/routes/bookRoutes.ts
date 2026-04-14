94cef0b897da20f0baa817a002fced2fd10f7f91
IvanBanal <ibanal@rrc.ca>
2026-04-14 12:51:06 -0500

    Added roles needed for memberRoutes.ts.

--- src/api/v1/routes/bookRoutes.ts
+++ src/api/v1/routes/bookRoutes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import * as controller from "../controllers/bookController";
 import { validateRequest } from "../middleware/validate";
 import { createBookSchema } from "../validation/bookSchema";
+import authenticate from "../middleware/authenticate";
+import isAuthorized from "../middleware/authorize";
 
 const router = Router();
 
