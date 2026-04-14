f283f4ca166c82cd2354cfba468b12aa3bbaadc6
IvanBanal <ibanal@rrc.ca>
2026-04-14 11:16:33 -0500

    Adding 3 routes needed for bookRoutes.ts.

--- src/api/v1/routes/bookRoutes.ts
+++ src/api/v1/routes/bookRoutes.ts
@@ -1,4 +1,12 @@
 import { Router } from "express";
 import * as controller from "../controllers/bookController";
 import { validateRequest } from "../middleware/validate";
-import { createEventSchema } from "../validation/eventSchema";
+import { createBookSchema } from "../validation/bookSchema";
+
+const router = Router();
+
+router.post("/", validateRequest({ body: createBookSchema }), controller.createBook);
+router.get("/", controller.getAllBooks);
+router.get("/:id", controller.getBookById);
+
+export default router;
