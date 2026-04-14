50de012ace03d8309cb3104a7027756fd56cc051
IvanBanal <ibanal@rrc.ca>
2026-04-14 07:55:43 -0500

    Updating HTTP_STATUS import path for validateMiddleware.ts.

--- src/api/v1/middleware/validateMiddleware.ts
+++ src/api/v1/middleware/validateMiddleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { ObjectSchema } from "joi";
-import { HTTP_STATUS } from "../../../constants/httpConstants"
+import { HTTP_STATUS } from "../constants/httpConstants"
 
 /**
  * Validation for middleware.
