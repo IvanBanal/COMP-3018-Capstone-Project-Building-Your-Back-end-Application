7debbf62bde853d31b49e4e2acfe48746286028a
IvanBanal <ibanal@rrc.ca>
2026-04-19 18:00:50 -0500

    Adding deleteMemberRepo function for memberRepository.ts.

--- src/api/v1/repositories/memberRepository.ts
+++ src/api/v1/repositories/memberRepository.ts
@@ -48,3 +48,17 @@ export const updateMemberRepo = async (id: string, data: Partial<Member>): Promi
     const updateDoc = await docRef.get();
     return updateDoc.data() as Member;
 };
+
+/**
+ * This will delete a member by ID in Firestore.
+ * @param id - Member ID.
+ * @returns True if deleted, false if not found.
+ */
+export const deleteMemberRepo = async (id: string): Promise<Boolean> => {
+    const docRef = db.collection(COLLECTION).doc(id);
+    const doc = await docRef.get();
+    if (!doc.exists) return false;
+
+    await docRef.delete();
+    return true;
+};
